5f2463b2ab16572b33e0c216efc9709f11f596ad
Laura-html <lrichards023@gmail.com>
2025-04-26 17:02:44 +0100

    added javascript for each city

---
+++ script.js
@@ -0,0 +1,17 @@
+setInterval(function () {
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format("H:mm:ss");
+}, 1000);
+
+setInterval(function () {
+  let marylandElement = document.querySelector("#maryland");
+  let marylandDateElement = marylandElement.querySelector(".date");
+  let marylandTimeElement = marylandElement.querySelector(".time");
+  let marylandTime = moment().tz("America/New_York");
+  marylandDateElement.innerHTML = marylandTime.format("MMMM Do YYYY");
+  marylandTimeElement.innerHTML = marylandTime.format("H:mm:ss");
+}, 1000);
